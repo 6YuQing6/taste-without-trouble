5cf9a966faaaca56ceddf2f79f6165995a3ea9ab
Sunny Han <shan66@ucsc.edu>
2026-01-06 00:26:37 -0800

    keys

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,14 +3,24 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+//   measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyB4Tt3r6emXGw1d9-ITKIy0LV8MOdpBxOQ",
+  authDomain: "science-on-the-road.firebaseapp.com",
+  projectId: "science-on-the-road",
+  storageBucket: "science-on-the-road.firebasestorage.app",
+  messagingSenderId: "982485359228",
+  appId: "1:982485359228:web:a056d8327ce3f44ee147f9",
+  measurementId: "G-J299613GBM",
 };
 
 const app = initializeApp(firebaseConfig);
