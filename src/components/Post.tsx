6137181c63182bb0ede062f4eb35af93335c3a9b
Sunny Han <shan66@ucsc.edu>
2026-02-23 23:06:11 -0800

    fix link issue home page

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,11 +18,18 @@ export default function Post({
     month: "long",
     day: "numeric",
   };
+  const formattedType =
+    type.toLowerCase() === "recipe"
+      ? "recipes"
+      : type.toLowerCase() === "article"
+        ? "articles"
+        : "";
 
+  const fullPath = formattedType ? `/${formattedType}/${link}` : `/${link}`;
   return (
     <li className="w-full border-b border-gray-200 pb-10 mb-10 list-none">
       <Link
-        to={link}
+        to={fullPath}
         className="group flex flex-col md:flex-row md:items-center md:gap-8">
         {/* Text Content (LEFT) */}
         <div className="flex-1 order-2 md:order-1 mt-6 md:mt-0">
